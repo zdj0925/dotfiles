4c4d379008ffb43dabd22584737cdd79560b857e
zdj925@gmail.com <zdj925@gmail.com>
2021-08-19 21:16:20 +0800

    add get_vim_plugin.sh and list

---
+++ get_vim_plugin.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+[ -z "$VIM_PLUGIN_LIST" ] && VIM_PLUGIN_LIST="vim_plugin.list"
+AStart=()
+AOpt=()
+
+get_list() {
+    for LINE in `cat $VIM_PLUGIN_LIST`
+    do
+        if [ $LINE == "start:" ]; then
+            FLAG=1;
+            continue;
+        elif [ $LINE == "opt:" ]; then
+            FLAG=2;
+            continue;
+        fi
+        if [ $FLAG -eq 1 ]; then
+            AStart[${#AStart[*]}]=$LINE
+        elif [ $FLAG -eq 2 ]; then
+            AOpt[${#AOpt[*]}]=$LINE
+        fi
+    done
+    
+    echo "AStart"
+    echo ${AStart[@]}
+    echo "AOpt"
+    echo ${AOpt[@]}
+
+    ret="$?"
+}
+git_submodule_add () {
+    uri="https://github.com"
+    start_path=".vim/pack/$LOGNAME/start"
+    opt_path=".vim/pack/$LOGNAME/start"
+    for PLUG in ${AStart[@]}
+    do
+       git submodule add  $uri/$PLUG $start_path/${PLUG##*/}
+    done
+
+    for PLUG in ${Aopt[@]}
+    do
+       git submodule add  $uri/$PLUG $opt_path/${PLUG##*/}
+    done
+}
+main () {
+    get_list
+    git_submodule_add
+}
+export http_proxy="172.23.220.1:1080"
+export https_proxy="172.23.220.1:1080"
+main
